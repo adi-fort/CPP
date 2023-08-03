cda1f543fa379b505d2a61db24d41feea0fec739
Andrea Di fortunato <adi-fort@e3r3p5.42roma.it>
2023-08-03 17:57:07 +0200

    start PhoneBook.cpp

--- CPP00/ex01/PhoneBook.cpp
+++ CPP00/ex01/PhoneBook.cpp
@@ -6,10 +6,42 @@
 /*   By: adi-fort <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/01 12:12:26 by adi-fort          #+#    #+#             */
-/*   Updated: 2023/08/02 11:12:44 by adi-fort         ###   ########.fr       */
+/*   Updated: 2023/08/03 17:56:31 by adi-fort         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PhoneBook.hpp"
 
+PhoneBook::PhoneBook()
+{
+	this->_index = 0;
+	return ;
+}
 
+~PhoneBook::PhoneBook()
+{
+	return ;
+}
+
+void	add_contact()
+{	
+	std::string	input;
+	this->_index++;
+	std::cout << "You have to enter in order: First name, last name, 
+	std::cout << "nickname, phone number and darkest secret; then press Enter!"; 
+	std::getline(std::cin, input);
+	std::getline(std::cin, input);
+	std::getline(std::cin, input);
+	std::getline(std::cin, input);
+	std::getline(std::cin, input);
+}
+
+void	replace_string()
+{
+
+}
+
+void	display_contacts()
+{
+
+}

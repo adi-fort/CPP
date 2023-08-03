4a529b9a5b71ae7f89138347cfe13144d3121b9d
Andrea Di fortunato <adi-fort@e3r3p5.42roma.it>
2023-08-03 17:14:36 +0200

    set and get

--- CPP00/ex01/Contact.cpp
+++ CPP00/ex01/Contact.cpp
@@ -6,7 +6,7 @@
 /*   By: adi-fort <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/01 12:12:11 by adi-fort          #+#    #+#             */
-/*   Updated: 2023/08/03 16:16:15 by adi-fort         ###   ########.fr       */
+/*   Updated: 2023/08/03 17:14:06 by adi-fort         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,57 @@ Contact::~Contact();
 	return ;
 }
 
+void	set_first_name(std::string first_name)
+{
+	this ->_first_name = first_name;
+	return ;
+}
 
+void	set_last_name(std::string last_name)
+{
+	this ->_last_name = last_name;
+	return ;
+}
 
+void	set_nick_name(std::string last_name)
+{
+	this ->_nick_name = last_name;
+	return ;
+}
+
+void	set_phone_number(std::string phone_number)
+{
+	this ->_phone_number = phone_number;
+	return ;
+}
+
+void	set_darkest_secret(std::string darkest_secret)
+{
+	this ->_darkest_secret = darkest_secret;
+	return ;
+}
+
+std::string	Contact::get_first_name() const
+{
+	return (this->first_name);
+}
+
+std::string	Contact::get_last_name() const
+{
+	return (this->last_name);
+}
+
+std::string	Contact::get_nick_name() const
+{
+	return (this->nick_name);
+}
+
+std::string	Contact::get_phone_number() const
+{
+	return (this->phone_number);
+}
+
+std::string	Contact::get_darkest_secret() const
+{
+	return (this->darkest_secret();
+}

bb56a08975265d945c29f983d97c836c616864d8
Andrea Di fortunato <adi-fort@e3r3p5.42roma.it>
2023-09-12 15:19:13 +0200

    0104OK

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: adi-fort <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 17:45:48 by adi-fort          #+#    #+#             */
-/*   Updated: 2023/09/11 17:55:34 by adi-fort         ###   ########.fr       */
+/*   Updated: 2023/09/12 15:18:42 by adi-fort         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 
 int main(int ac, char	**argv)
 {
+	size_t	len;
 	std::string buffer;
 	std::stringstream stream;
 	std::ifstream file(argv[1]);
@@ -27,12 +28,26 @@ int main(int ac, char	**argv)
 		std::cout << "Error: incorrect number of parameters" << std::endl;
 		return (1);
 	}
+	std::string changestr = argv[2];
+	len = changestr.length();
 	stream << file.rdbuf();
 	buffer = stream.str();
-	std::getline(argv[1], argv[2]);
+	size_t pos = buffer.find(argv[2]);
+	if (argv[2] == argv[3])
+	{	
+		file1 << buffer;
+		file1.close();
+		file.close();
+		return (0);
+	}
+	while (pos != std::string::npos)
+	{
+		buffer.erase(pos, len);
+		buffer.insert(pos, argv[3]);
+		pos = buffer.find(argv[2]);
+	}
 	file1 << buffer;
-		
-
 	file1.close();
 	file.close();
+	return (0);
 }

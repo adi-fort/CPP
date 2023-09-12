f604121c9fd1f373ee553923e8a0b3937e302bb2
Andrea Di fortunato <adi-fort@e3r3p5.42roma.it>
2023-09-12 16:36:17 +0200

    0105

---
+++ CPP01/ex05/Harl.cpp
@@ -0,0 +1,38 @@
+# include "Harl.hpp"
+# include <iomainp>
+# include <string>
+
+Harl::Harl(void)
+{
+	return :
+}
+
+Harl::~Harl(void)
+{
+	return;
+}
+
+void	debug(void)
+{
+	std::cout << "I love having extra bacon." << endl;
+}
+
+void	info(void)
+{
+	std::cout << "I cannot believe adding extra bacon costs more money." << endl;
+}
+
+void	warning(void)
+{
+	std::cout << "I think I deserve to have some extra bacon for free" << endl;
+}
+
+void	error(void)
+{
+	std::cout << "I want to speak to the manager now." << enld;
+}
+
+void	complain(std::string level)
+{
+	
+}

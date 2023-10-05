2a1a8385457e74a48602a6665fd08e5aa1ecd9f3
Andrea Di fortunato <adi-fort@e3r3p5.42roma.it>
2023-10-05 17:11:54 +0200

    ready to push

--- CPP01/ex02/main.cpp
+++ CPP01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: adi-fort <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/31 17:01:31 by adi-fort          #+#    #+#             */
-/*   Updated: 2023/08/31 17:24:37 by adi-fort         ###   ########.fr       */
+/*   Updated: 2023/10/05 17:02:20 by adi-fort         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,23 @@ int main()
 	std::string &stringREF = string;		//reference to variable
 
 	string = "HI THIS IS BRAIN";		
-	std::cout << &string << std::endl;		//memory address string
-	std::cout << stringPTR << std::endl;	//memory address pointer
-	std::cout << &stringREF << std::endl;	//memory address reference
 	
+	std::cout << "string memory address: ";
+	std::cout << &string << std::endl;		//memory address string
+	std::cout << std::endl;	
+	std::cout << "pointer memory address: ";
+	std::cout << stringPTR << std::endl;	//memory address pointer
+	std::cout << std::endl;	
+	std::cout << "reference memory address: ";
+	std::cout << &stringREF << std::endl;	//memory address reference
+	std::cout << std::endl;	
+	std::cout << "value stored in string: ";
 	std::cout << string << std::endl;		//value stored in string
+	std::cout << std::endl;	
+	std::cout << "pointer to variable string: ";
 	std::cout << *stringPTR << std::endl;	//value pointed by pointer with *
+	std::cout << std::endl;	
+	std::cout << "reference to variable string: ";
 	std::cout << stringREF << std:: endl;	//value pointed by ref.
 	
 	return (0);
